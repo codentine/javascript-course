d87ac6913455463a38bd235049a45a5384d28647
Alfonso Banda <alfonso@yalochat.com>
2020-10-14 00:03:24 -0500

    Cap2 - variables

---
+++ variables.js
@@ -0,0 +1,23 @@
+// variables
+var nombre = "Jose".substr()
+let edad = 25.32
+edad = 30
+
+/*
+Number()
+String()
+Array()
+Object()
+Boolean()
+*/
+if(true) {
+  let x = 30
+  var nombre = "Alfonso"
+}
+
+edad = "pepe"
+// constantes
+const permiso = true
+
+
+console.log(edad)
