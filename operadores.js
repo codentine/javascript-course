84c5c1a6d81854b6a8a025df73a3eb4449d25849
Alfonso Banda <alfonso@yalochat.com>
2020-10-19 22:16:49 -0500

    Cap3 - operadores

---
+++ operadores.js
@@ -0,0 +1,35 @@
+//Unario
+const nombre = "Alfonso"
+// console.log(typeof 2)
+
+// Binarios
+// Aritmeticos
+// + - / *  %  **
+// console.log(2 ** 3)
+// console.log(7 % 2)
+
+// Relacionales
+// >   <   >=   <= !== ===
+// console.log(3 === "3")
+
+// Logicos
+/*
+  AND &&
+  T   T    T
+  T   F    F
+  F   T    F
+  F   F    F
+
+  OR ||
+  T   T    T
+  T   F    T
+  F   T    T
+  F   F    F
+*/
+//console.log(aux)
+
+// Ternario
+const aux = 18 > 15 ? "ES verdadero" : "Es Falso"
+console.log(aux)
+
+
