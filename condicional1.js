db3fd53c749441983a5300c7e5473bbd7def7cdf
Alfonso Banda <alfonso@yalochat.com>
2020-10-20 00:02:59 -0500

    Cap4 -If/else

---
+++ condicional1.js
@@ -0,0 +1,14 @@
+const edad = 16
+
+if(edad >= 18) {
+  console.log("Puede Manejar un Auto")
+} else if(edad >= 16) {
+  console.log("Tiene Permiso condicionado")
+} else {
+  console.log("No cumple con la edad minima")
+}
+
+
+
+console.log("Fin del codigo")
+
