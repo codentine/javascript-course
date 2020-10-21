bb35e79a15573abcf0c28e8a92466202330a37b4
Alfonso Banda <alfonso@yalochat.com>
2020-10-20 23:45:36 -0500

    Cap5 - switch

---
+++ condicional2.js
@@ -0,0 +1,12 @@
+const color = "azul"
+
+switch(color) {
+  case "rojo":
+    console.log("Tu color favorito es el rojo")
+    break
+  case "azul":
+    console.log(" El mar es azul")
+    break
+  default:
+    console.log("tu color no se encuentra en el menu")
+}
