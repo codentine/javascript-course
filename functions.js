964ade08416f7fa41060804ba048854aa4b7ea5a
Alfonso Banda <alfonso@yalochat.com>
2020-11-25 00:14:44 -0600

    Cap9 Funciones

---
+++ functions.js
@@ -0,0 +1,18 @@
+// Forma 1
+function saludar() {
+  console.log(" Hola mundo ")
+}
+
+// Forma 2
+const saludar2 = function() {
+  console.log(" Hola mundo 2 ")
+}
+
+// Forma 3
+const saludar3 = () => {
+  console.log(" Hola mundo 3 ")
+}
+
+saludar()
+saludar2()
+saludar3()
