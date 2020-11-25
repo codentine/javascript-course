1dec935f8a84aaafec6de4c80f2d5e9303955ff1
Alfonso Banda <alfonso@yalochat.com>
2020-11-25 00:15:05 -0600

    Capp 11 scope

---
+++ function-scope.js
@@ -0,0 +1,11 @@
+
+let apellido = 'Banda'
+
+function saludar() {
+  let nombre = 'Alfonso'
+  let apellido = 'Reyna'
+  console.log('Hola: ',nombre, apellido)
+}
+
+saludar()
+console.log(apellido)
